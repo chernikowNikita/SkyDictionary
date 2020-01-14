5d3ccf3b47b2d567e3301b742350c89e53175382
Nikita MacBook <n.chernikov@kingbird.ru>
2020-01-14 19:22:40 +0300

    Create Project

---
+++ Podfile
@@ -0,0 +1,16 @@
+use_frameworks!
+platform :ios, '12.0'
+
+target 'SkyDictionary' do
+  # core RxSwift
+  pod 'RxSwift', '4.4.1'
+  pod 'RxCocoa', '4.4.1'
+  pod 'RxDataSources', '~> 3.1'
+
+  # Community projects
+  pod 'Action', '~> 3.9'
+  
+  # API
+  pod 'Moya/RxSwift', '~> 13.0'
+  
+end
---
+++ SkyDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyDictionary
+//
+//  Created by Никита Черников on 14/01/2020.
+//  Copyright © 2020 Никита Черников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
