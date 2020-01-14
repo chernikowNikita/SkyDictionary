12794b95be26a06fb84c428cac0f2c7f1b9dc9cc
Nikita MacBook <n.chernikov@kingbird.ru>
2020-01-14 21:17:10 +0300

    SD-1 Configure api service

--- SkyDictionary/groups/search/SearchVC.swift
+++ SkyDictionary/groups/search/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  SkyDictionary
 //
 //  Created by Никита Черников on 14/01/2020.
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
-
+class SearchVC: UIViewController {
+    
+    // MARK: - Public properties
+    var viewModel: SearchVM!
+    
+    // MARK: - Private properties
+    
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        
+        configureDataSource()
+        
+        setEditing(true, animated: false)
     }
+    
+    // MARK: - Setup View
+    private func setupView() {
+        
+    }
+    
+    private func configureDataSource() {
+        
+    }
+    
+}
 
-
+extension SearchVC: BindableType {
+    
+    func bindViewModel() {
+        
+    }
+    
 }
 
---
+++ SkyDictionary/groups/search/SearchVM.swift
@@ -0,0 +1,13 @@
+//
+//  SearchVM.swift
+//  SkyDictionary
+//
+//  Created by Никита Черников on 14/01/2020.
+//  Copyright © 2020 Никита Черников. All rights reserved.
+//
+
+import Foundation
+
+class SearchVM {
+    
+}
